e1ceed7a535ab07aa72ab6725f3604b28ef50ba9
777uliahshafar <777uliahshafar@gmail.com>
2024-05-18 16:20:19 +0800

    newbeginning

--- footer.php
+++ footer.php
@@ -1,61 +1,63 @@
-<?php
-/**
- * Footer template
- *
- * @package Aquila
- */
-?>
+<footer class="site-footer">
+      <div class="site-footer__inner container container--narrow">
+        <div class="group">
+          <div class="site-footer__col-one">
+            <h1 class="school-logo-text school-logo-text--alt-color">
+              <a href="#"><strong>Fictional</strong> University</a>
+            </h1>
+            <p><a class="site-footer__link" href="#">555.555.5555</a></p>
+          </div>
 
+          <div class="site-footer__col-two-three-group">
+            <div class="site-footer__col-two">
+              <h3 class="headline headline--small">Explore</h3>
+              <nav class="nav-list">
+                <ul>
+                  <li><a href="#">About Us</a></li>
+                  <li><a href="#">Programs</a></li>
+                  <li><a href="#">Events</a></li>
+                  <li><a href="#">Campuses</a></li>
+                </ul>
+              </nav>
+            </div>
 
-<footer id="site-footer" class="bg-light p-4">
-	<div class="container color-gray">
-		<div class="row">
-			<section class="col-lg-4 col-md-6 col-sm-12">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid cum cupiditate, dolores doloribus eos
-				facere fugit in ipsum nisi, nostrum obcaecati, optio porro quidem reiciendis sunt unde ut vitae.
-				Aperiam.
-			</section>
-			<section class="col-lg-4 col-md-6 col-sm-12">
-				<?php if ( is_active_sidebar( 'sidebar-2' ) ) { ?>
-					<aside>
-						<?php dynamic_sidebar( 'sidebar-2' ); ?>
-					</aside>
-				<?php } ?>
-			</section>
-			<section class="col-lg-4 col-md-6 col-sm-12">
-				<ul class="d-flex">
-					<li class="list-unstyled">
-						<a href="https://www.facebook.com/codeytek" title="facebook" target="_blank">
-							<svg width="48">
-								<use href="#icon-facebook"></use>
-							</svg>
-						</a>
-					</li>
-					<li class="list-unstyled">
-						<a href="https://www.linkedin.com/" title="linkedin" target="_blank">
-							<svg width="48">
-								<use href="#icon-linkedin"></use>
-							</svg>
-						</a>
-					</li>
-					<li class="list-unstyled">
-						<a href="https://twitter.com/codeytek" title="twitter" target="_blank">
-							<svg width="48">
-								<use href="#icon-twitter"></use>
-							</svg>
-						</a>
-					</li>
-				</ul>
-			</section>
-		</div>
-	</div>
-</footer>
-</div>
-</div>
-<?php
-get_template_part( 'template-parts/content', 'svgs' );
-wp_footer();
-?>
+            <div class="site-footer__col-three">
+              <h3 class="headline headline--small">Learn</h3>
+              <nav class="nav-list">
+                <ul>
+                  <li><a href="#">Legal</a></li>
+                  <li><a href="#">Privacy</a></li>
+                  <li><a href="#">Careers</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+
+          <div class="site-footer__col-four">
+            <h3 class="headline headline--small">Connect With Us</h3>
+            <nav>
+              <ul class="min-list social-icons-list group">
+                <li>
+                  <a href="#" class="social-color-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                </li>
+                <li>
+                  <a href="#" class="social-color-twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                </li>
+                <li>
+                  <a href="#" class="social-color-youtube"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+                </li>
+                <li>
+                  <a href="#" class="social-color-linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                </li>
+                <li>
+                  <a href="#" class="social-color-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <?php wp_footer(); ?>
 </body>
 </html>
-
--- index.php
+++ index.php
@@ -1,72 +1,113 @@
-<?php
-/**
- * Main template file.
- *
- * @package Aquila
- */
-
-get_header();
+<?php get_header();
 
 ?>
 
-	<div id="primary">
-		<main id="main" class="site-main mt-5" role="main">
-			<?php
-			if ( have_posts() ) :
-				?>
-				<div class="container">
-					<?php
-					if ( is_home() && ! is_front_page() ) {
-						?>
-						<header class="mb-5">
-							<h1 class="page-title screen-reader-text">
-								<?php single_post_title(); ?>
-							</h1>
-						</header>
-						<?php
-					}
-					?>
+<div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(images/library-hero.jpg)"></div>
+      <div class="page-banner__content container t-center c-white">
+        <h1 class="headline headline--large">Welcome!</h1>
+        <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
+        <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
+        <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
+      </div>
+    </div>
 
-					<div class="row">
-						<?php
-						$index         = 0;
-						$no_of_columns = 3;
+    <div class="full-width-split group">
+      <div class="full-width-split__one">
+        <div class="full-width-split__inner">
+          <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
-						while ( have_posts() ) : the_post();
+          <div class="event-summary">
+            <a class="event-summary__date t-center" href="#">
+              <span class="event-summary__month">Mar</span>
+              <span class="event-summary__day">25</span>
+            </a>
+            <div class="event-summary__content">
+              <h5 class="event-summary__title headline headline--tiny"><a href="#">Poetry in the 100</a></h5>
+              <p>Bring poems you&rsquo;ve wrote to the 100 building this Tuesday for an open mic and snacks. <a href="#" class="nu gray">Learn more</a></p>
+            </div>
+          </div>
+          <div class="event-summary">
+            <a class="event-summary__date t-center" href="#">
+              <span class="event-summary__month">Apr</span>
+              <span class="event-summary__day">02</span>
+            </a>
+            <div class="event-summary__content">
+              <h5 class="event-summary__title headline headline--tiny"><a href="#">Quad Picnic Party</a></h5>
+              <p>Live music, a taco truck and more can found in our third annual quad picnic day. <a href="#" class="nu gray">Learn more</a></p>
+            </div>
+          </div>
 
-							if ( 0 === $index % $no_of_columns ) {
-								?>
-								<div class="col-lg-4 col-md-6 col-sm-12">
-								<?php
-							}
+          <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
+        </div>
+      </div>
+      <div class="full-width-split__two">
+        <div class="full-width-split__inner">
+          <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
 
-							get_template_part( 'template-parts/content' );
+          <div class="event-summary">
+            <a class="event-summary__date event-summary__date--beige t-center" href="#">
+              <span class="event-summary__month">Jan</span>
+              <span class="event-summary__day">20</span>
+            </a>
+            <div class="event-summary__content">
+              <h5 class="event-summary__title headline headline--tiny"><a href="#">We Were Voted Best School</a></h5>
+              <p>For the 100th year in a row we are voted #1. <a href="#" class="nu gray">Read more</a></p>
+            </div>
+          </div>
+          <div class="event-summary">
+            <a class="event-summary__date event-summary__date--beige t-center" href="#">
+              <span class="event-summary__month">Feb</span>
+              <span class="event-summary__day">04</span>
+            </a>
+            <div class="event-summary__content">
+              <h5 class="event-summary__title headline headline--tiny"><a href="#">Professors in the National Spotlight</a></h5>
+              <p>Two of our professors have been in national news lately. <a href="#" class="nu gray">Read more</a></p>
+            </div>
+          </div>
 
-							$index ++;
+          <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
+        </div>
+      </div>
+    </div>
 
-							if ( 0 !== $index && 0 === $index % $no_of_columns ) {
-								?>
-								</div>
-								<?php
-							}
+    <div class="hero-slider">
+      <div data-glide-el="track" class="glide__track">
+        <div class="glide__slides">
+          <div class="hero-slider__slide" style="background-image: url(images/bus.jpg)">
+            <div class="hero-slider__interior container">
+              <div class="hero-slider__overlay">
+                <h2 class="headline headline--medium t-center">Free Transportation</h2>
+                <p class="t-center">All students have free unlimited bus fare.</p>
+                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="hero-slider__slide" style="background-image: url(images/apples.jpg)">
+            <div class="hero-slider__interior container">
+              <div class="hero-slider__overlay">
+                <h2 class="headline headline--medium t-center">An Apple a Day</h2>
+                <p class="t-center">Our dentistry program recommends eating apples.</p>
+                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="hero-slider__slide" style="background-image: url(images/bread.jpg)">
+            <div class="hero-slider__interior container">
+              <div class="hero-slider__overlay">
+                <h2 class="headline headline--medium t-center">Free Food</h2>
+                <p class="t-center">Fictional University offers lunch plans for those in need.</p>
+                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="slider__bullets glide__bullets" data-glide-el="controls[nav]"></div>
+      </div>
+    </div>
 
-						endwhile;
-						?>
-					</div>
-				</div>
-			<?php
 
-			else :
+<?php get_footer();
 
-				get_template_part( 'template-parts/content-none' );
-
-			endif;
-
-			aquila_pagination();
-			?>
-		</main>
-	</div>
-
-<?php
-get_footer();
+?>
 
